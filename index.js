4a2719d0374927728e165e10cd2dc9005793e8d3
Jean Pierre <hjepih@gmail.com>
2020-12-02 08:55:08 -0300

    feat: encrypt

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv/config");
+
+const knex = require('knex')
+const bcrypt = require('bcryptjs');
+
+let i = 0;
+
+const db = knex({
+    client: process.env.CLIENT,
+    connection: {
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABSE
+    }
+});
+
+async function allData(){
+    return await db
+                .select("*")
+                .from("users")
+                .whereNull("password")
+}
+
+async function updateData({id, senha, email, username}){
+    i++;
+    console.log(`Total de dados carregados: ${i}`);
+
+    const hash = bcrypt.hashSync(senha, 12);
+
+    const modify = await db("users").update({password: hash}).where({id: id});
+
+    console.log(`Change ${modify} = ${username} - ${email} (${id}): ${senha} -> ${hash}`);
+}
+
+(async function(){
+    const data = await allData();
+
+    data.forEach(updateData);
+})();

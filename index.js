097f4366aefac117865561dbd026758a195317d3
Jean Pierre <hjepih@gmail.com>
2020-12-02 09:42:42 -0300

    fix: database name

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = knex({
         host: process.env.HOST,
         user: process.env.USER,
         password: process.env.PASSWORD,
-        database: process.env.DATABSE
+        database: process.env.DATABASE
     }
 });
 
